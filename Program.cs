cf57d92f30342a8afbdee726f03c172da9d23385
Logan Yagadics <zynosstudios.logan@gmail.com>
2022-07-30 16:07:12 -0400

    first commit2

--- Program.cs
+++ Program.cs
@@ -3,22 +3,36 @@ using gomedsite.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Identity;
+using AspNetCore.Identity.Mongo;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Policy;
+using SampleSite.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentityMongoDbProvider<TestSiteUser>(identity =>
+    {
+        identity.Password.RequireDigit = false;
+        identity.Password.RequireLowercase = false;
+        identity.Password.RequireNonAlphanumeric = false;
+        identity.Password.RequireUppercase = false;
+        identity.Password.RequiredLength = 1;
+        identity.Password.RequiredUniqueChars = 0;
+    },
+    mongo =>
+    {
+        mongo.ConnectionString = connectionString;
+    };
+
+builder.Services.AddSingleton<IAuthorizationPolicyProvider, AuthorizationPolicyProvider>();
+builder.Services.AddSingleton<IAuthorizationHandler, HasClaimHandler>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
@@ -44,8 +58,12 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=User}/{action=Index}/{id?}");
+
 app.MapBlazorHub();
+app.MapRazorPages();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
